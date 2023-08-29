b6180c64588a8d8afea83ccb32afd749ba399c57
Dayarat <kavindusandaruwan703@gmail.com>
2023-08-29 20:48:54 +0530

    backend connect to the chatengine

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,20 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try{
+     const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      {username: username, secret: username, first_name: username},
+      {headers: {"Private-Key": "d2ea221e-228b-4f12-9401-c19604edca55" } }
+     );
+
+    return res.status(r.status).json(r.data);
+  }catch(e){
+    return res.status(e.response.status).json(e.response.data);
+  }
+  
+  // return res.json({ username: username, secret: "sha256..." });
 });
 
 app.listen(3001);
