0d1b9c63ace8a2843047538392781029e546882d
Dayarat <kavindusandaruwan703@gmail.com>
2023-08-29 23:58:20 +0530

    created fronend

--- frontend/src/ChatPage.jsx
+++ frontend/src/ChatPage.jsx
@@ -1,5 +1,20 @@
-const ChatsPage = () => {
-    return<>chats...</>
-} 
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 
-export default ChatsPage;
+const ChatsPage = (props) => {
+
+    return(
+    <div style={{height: '100vh'}}>
+        <PrettyChatWindow
+            projectId= "75290f72-6bb7-4b90-a8c8-22930800e9d7"
+            username={props.user.username}
+            secret={props.user.secret}
+            style={{height: '100%'}}
+        />
+    </div>
+    );
+} ;
+
+// const ChatsPage = () => {
+//     return <div className="background">chats...</div>;
+//   }
+  export default ChatsPage;
